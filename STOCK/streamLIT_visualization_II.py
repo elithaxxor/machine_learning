18c3246df0ac6dd7b6f50d3ac725c21295291a85
a-robot <86808805+elithaxxor@users.noreply.github.com>
2022-01-15 19:35:49 -0800

    Update streamLIT_visualization_II.py

--- STOCK/streamLIT_visualization_II.py
+++ STOCK/streamLIT_visualization_II.py
@@ -10,12 +10,20 @@ import pandas_datareader as web
 import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from yahoo_fin import stock_info as si
+from yahoo_fin import news
+from yahoo_fin import options
+import csv
+import pickle
+
+import traceback
 
 
 image = Image.open('wsb.png')
 st.image(image, use_column_width=True)
 st.write('''
-    #  webapp to display *opening* and **closing** of stock prices.
+    #  Why *Invest* when you could **SPECULATE**! [copyleft - all wrongs reserved] - 
+    https://github.com/elithaxxor?tab=repositories
     ***  ''')
 
 ## preprocessing
@@ -42,29 +50,142 @@ def additional_data(tickerSymbol):
     df = web.DataReader(tickerSymbol, f'yahoo', start, end)
     df['Pct Change'] = df['Adj Close'].pct_change()
     df['Stock Return'] = (df['Pct Change'] + 1).cumprod()
+
+   # st.sidebar.header('User Selections')
+    #selected_year = st.sidebar.selectbox('Year', list(reversed(range(2022-1990))))
+
     #df = df[['Open', 'High', 'Low', 'Close']]
     #df['Date'] = df['Date'].map(mdates.date2num)
+
+
     df.reset_index(inplace=True)
     st.subheader(' [5-DAY] Stock Information with PCT change and Return.')
     st.write(df.head())
     print(df.head())
 
-    # save csv and let user download
+    ## more info (quote table)
+    quote_table = si.get_quote_table(tickerSymbol)
+    quote_df = pd.DataFrame.from_dict([quote_table])
+    financials = si.get_financials(tickerSymbol)
+    financials_df = pd.DataFrame.from_dict([financials])
+    st.header(f'[Current Stock] {tickerSymbol}---{dt.datetime.now()}]')
+    st.write(quote_df.head())
+    st.write(dict(quote_table))
+
+    valuation = si.get_stats_valuation(tickerSymbol)
+    splits = si.get_splits(tickerSymbol)
+    next_earnings = si.get_next_earnings_date(tickerSymbol)
+    stat = si.get_stats(tickerSymbol)
+
+    # save csv and let user download (BUTTONS)
+    #try:
     df.to_csv(f"pct change + {tickerSymbol} + {dt.date.today()}.csv")  # Write df Object to .CSV
+    quote_df.to_csv( f"Stock_Quotes + {tickerSymbol} + {dt.date.today()}.csv")
+
+    # save locations
     saved_df1 = f"pct change + {tickerSymbol} + {dt.date.today()}.csv"
-    with open(saved_df1, 'rb') as f:
-        st.download_button(label='Download Stock Csv',data=f,  file_name=saved_df1, on_click=True)
+    saved_df2 = f"Stock_Quotes + {tickerSymbol} + {dt.date.today()}.csv"
+    table_save_loc = f"quote_table + {tickerSymbol} + {dt.date.today()}.csv"
+    financials_loc = f"financials  + {tickerSymbol} + {dt.date.today()}.csv"
+    stats_loc =  f"Stock Stats + {tickerSymbol} + {dt.date.today()}.csv"
+    splits_loc = f"Stock splits + {tickerSymbol} + {dt.date.today()}.csv"
+    valuation_loc = f"Valuation + {tickerSymbol} + {dt.date.today()}.csv"
+
+    try:
+        table_colums = []
+        financial_columns = []
+        with open(table_save_loc, 'w') as csvfile:
+            writer = csv.DictWriter(fieldnames=table_colums)
+            writer.writeheader()
+            for data in table_save_loc:
+                writer.writerow(data)
+
+        with open(financials_loc, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=financial_columns)
+            writer.writeheader()
+            for data in table_save_loc:
+                writer.writerow(data)
+    except IOError:
+        print("I/O error"); pass
+    except Exception as e:
+        print(f'Exception in Dictionary -- csv writer. \, {str(e)} \n {traceback.format_exc()}')
+        pass
+
+    try:
+        ## context manager / download-button
+        with open(saved_df1, 'rb') as f:
+            st.download_button(label='Download Stock Csv',data=f, file_name=saved_df1, on_click=True)
+        with open(saved_df2, 'rb') as f2:
+            st.download_button(label='Stock Quote Info',data=f2, file_name=saved_df2, on_click=True)
+
+        # write dict to file / download button
 
 
-    st.header(f'[{tickerSymbol}] [Pct Change] [{(df["Pct Change"].count())} Values] ') #volume
+        with open(table_save_loc, 'rb') as f4:
+            table_save = pickle.dumps(quote_table)
+            st.download_button(label='Financials', data=f4, file_name=table_save_loc, on_click=True)
+
+        with open(financials_loc, 'rb') as f5:
+            financials = pickle.dumps(financials)
+            st.download_button(label='Financial Docs', data=f5, file_name=financials_loc, on_click=True)
+
+    except Exception as e:
+        print(f'Data-File Writing \n {str(e)}, {traceback.format_exc()}')
+        pass
+
+    print(df.head())
+    print(quote_df.head())
+
+    ## earnings, split ratio, stats, valuation
+    st.header(f'[{tickerSymbol}] [Equity Info]- **Earnings, Stats, Valuation ') #volume
+    st.write("Next Earnings:\n ", si.get_next_earnings_date(tickerSymbol),
+             "Split Ratio:\n ", si.get_splits(tickerSymbol),
+             "Stats\n", si.get_stats(tickerSymbol))
+             #"Valuation\n", si.get_stats_valuation(tickerSymbol))
+
+
+    try:
+        st.subheader('[Open] [Close] [Ask] [Pct Change]')
+        st.bar_chart(df['Open'], df['Close'], df['Pct Change'], float(quote_df['Avg. Volume']))
+        st.line_chart(df['Open'], df['Close'], quote_df['1y Target Est'], df['Pct Change'])
+    except Exception as e:
+        print(f'Exception in Open- graph\n {str(e)}, {traceback.format_exc()}')
+        pass
+
+    st.subheader(f'[{tickerSymbol}] [Pct Change] [{(df["Pct Change"].count())} Values] ') #volume
     st.line_chart(df['Pct Change'])
 
-    st.header(f'[{tickerSymbol}] [Stock Return] [{(df["Stock Return"].count())} Values] ') #stock return (df[stock_return]) above
+    st.subheader(f'[{tickerSymbol}] [Stock Return] [{(df["Stock Return"].count())}] [Values] ') #stock return (df[stock_return]) above
     st.line_chart(df['Stock Return'])
-    st.header(f'[{tickerSymbol}] [ALL TOGETHER] [{(df["Volume"].count())} Values]') # all days values together.
+    st.subheader(f'[{tickerSymbol}] [ALL TOGETHER] [{(df["Volume"].count())} Values]') # all days values together.
     st.line_chart(df)
 
+    print(financials_df.head())
+    print(type(financials_df))
+    st.header(f'[{tickerSymbol}] [Financials] {dt.datetime.now()}]')
+    st.write('Financials\n', financials)
+    try:
+        st.write(financials_df.head())
+    except:
+        pass
 
+
+    try:
+        moving_averages = [150, 200]
+        for ma in moving_averages:
+            df['SMA_' + str(ma)] = round(df['Adj Close'].rolling(window=ma).mean())
+            quote_df['SMA_' + str(ma)] = round(df['Adj Close'].rolling(window=ma).mean())
+            latest_price = df['Adj Close'][-1]
+            latest_price1 = quote_df['Adj Close'][-1]
+            print(latest_price)
+            pe_ratio = float(si.get_quote_table(tickerSymbol)['PE Ratio (TTM)'])
+    except Exception as e:
+        print(f'Exception in Open- graph\n {str(e)}, {traceback.format_exc()}')
+        pass
+
+    ## display ticker news
+    st.header(f'[{tickerSymbol}]--News')
+    st.text(news.get_yf_rss(tickerSymbol))
 
 def graph_display(tickerSymbol):
     ''' Takes User Input and displays graphs, accordingly '''
@@ -76,9 +197,11 @@ def graph_display(tickerSymbol):
 
     saved_df=f"Security_Info + {tickerSymbol} + {dt.date.today()}.csv"
     with open(saved_df,'rb') as f:
-        st.download_button(label='Download Stock Csv',data=f,  file_name=f"Security_Info + {tickerSymbol} + {dt.date.today()}.csv", on_click=True)
+        st.download_button(label='Download Stock Csv',data=f,  file_name=f"Security Info + {tickerSymbol} + {dt.date.today()}.csv", on_click=True)
 
-    st.subheader(f'{tickerSymbol} [Stock Information] ')
+
+    # display basic stock info
+    st.header(f'{tickerSymbol} [Stock Information] ')
     print(tickerDf.head())
     st.write(tickerDf.head())
 
@@ -96,7 +219,8 @@ def graph_display(tickerSymbol):
     st.header(f'[{tickerSymbol}] [Daily High] [{(tickerDf["High"].count())} Values] ') #volume
     st.line_chart(tickerDf.Volume)
 
-    additional_data(tickerSymbol) ## call function for addiotnal data
+    # call function for added info (financial docs, and Df that require preprocessing
+    # additional_data(tickerSymbol) ## call function for addiotnal data
 
 
 
